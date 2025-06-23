58f051b021002b5fa7dc1f764d44b9fd65c9e023
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-06-22 17:32:50 -0700

    Add basic Svelte frontend

---
+++ frontend/tunescout-frontend/src/lib/Counter.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let count: number = $state(0);
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<div class="buttons">
+  <button onclick={increment} class="button">
+    count is {count}
+  </button>
+
+  <button onclick={increment} class="button">
+    count is {count}
+  </button>
+</div>
+
+<style>
+  .buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
+  .button {
+    background-color: #ff3e00;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+</style>
