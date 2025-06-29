26e6de8a92b4898a81dc40695dfc6bcfb428e9a1
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-06-28 19:13:57 -0700

    Add React frontend with Vite

---
+++ frontend/src/auth/spotify.ts
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const generateRandomString = (length: number): string => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  console.log(values);
+
+  // starts with "", concats possible[x % possible.length] for each random value in values
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+const sha256 = async (plain: string): Promise<ArrayBuffer> => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input: ArrayBuffer): string => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+
+
+const pkceFlow = async (clientId: string, redirectUri: string, scope: string, authUrl: URL): Promise<void> => {
+    const codeVerifier = generateRandomString(128);
+    const hashed = await sha256(codeVerifier);
+    const codeChallenge = base64encode(hashed);
+
+    window.localStorage.setItem('code_verifier', codeVerifier);
+
+    const params = {
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirectUri,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge
+    };
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+}
+
+export default pkceFlow;
