c938ad1c8b4e59147ee37e23c67f39c47913e167
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-06-30 22:34:41 -0700

    Add callback and handle spotify code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Callback from './pages/Callback.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/callback",
+    element: <Callback />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ frontend/src/pages/Callback.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+
+const Callback = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Extract the authorization code from the URL
+        const params = new URLSearchParams(location.search);
+        const code = params.get('code');
+        if (code) {
+            // Store the code in local storage or handle it as needed
+            window.localStorage.setItem('auth_code', code);
+        } else {
+            // Handle the case where no code is present
+            console.error("Authorization code not found in the URL.");
+        }
+        navigate('/', {replace: true}); // Redirect to home or another page
+    }, [location, navigate]);
+
+    return (<div></div>)
+}
+
+export default Callback;
